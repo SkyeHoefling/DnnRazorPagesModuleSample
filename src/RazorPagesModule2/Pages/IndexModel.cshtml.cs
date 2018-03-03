868105256db09eeaf881e6dcc8041a4f91c42ea9
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-03-02 22:32:59 -0500

    Added 2nd razor module which is a carbon copy of the original

---
+++ src/RazorPagesModule2/Pages/IndexModel.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using DotNetNuke.Web.Mvc.RazorPages.SDK;
+
+namespace DNNTAG.RazorPagesModule2.Pages
+{
+    public class IndexModel : DnnPageModel
+    {        
+        
+        public IndexModel()
+        {            
+        }
+
+        // TODO - Add on Get and on post handlers, see - https://docs.microsoft.com/en-us/aspnet/core/mvc/razor-pages/?tabs=visual-studio
+
+        public string WelcomeMessage => "This is a different DNN Module built with Razor Pages";
+    }
+}
